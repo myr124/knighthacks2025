0acdb092aaf5896e3b7d9429346004dcc3137a1b
Jerry He <jerryhe365@gmail.com>
2025-10-25 16:43:01 -0400

    Adjusted dark mode for map

--- app/components/simulation-v2/PeriodMapView.tsx
+++ app/components/simulation-v2/PeriodMapView.tsx
@@ -157,11 +157,11 @@ function ThemeTileLayer() {
     <TileLayer
       key={isDark ? 'dark' : 'light'}
       attribution={isDark
-        ? '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        ? '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         : '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }
       url={isDark
-        ? 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+        ? 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
         : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       }
     />
